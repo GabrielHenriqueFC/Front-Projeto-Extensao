11c3ac85b1446e47cc5bf471ab06d8feefaf3662
Itaji de Carvalho <itaji.1999@gmail.com>
2024-11-06 11:32:37 -0300

    corrige rota cadastro

--- Front-P-E/src/Rotas/Enter.tsx
+++ Front-P-E/src/Rotas/Enter.tsx
@@ -13,7 +13,7 @@ export const Enter = () => {
             <p className="subtitle">Let’s start your English adventure <br/> today. Learning English is fun!</p>
         </main>
         <section>
-            <Link to='/cadastro' className="button button-primary" type="button">COMEÇAR AGORA</Link>
+            <Link to='/register' className="button button-primary" type="button">COMEÇAR AGORA</Link>
             <Link to='/login' className="button button-secondary" type="button">JÁ TENHO CONTA</Link>
         </section>
     </div>
